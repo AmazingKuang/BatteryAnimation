0766f4832e23da0bc2288e163978350c5dff3243
GAPSD <13798187289@163.com>
2018-10-23 11:42:58 +0800

    Initial Commit

---
+++ BatteryAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BatteryAnimation
+//
+//  Created by GAPSD on 2018/10/23.
+//  Copyright © 2018年 GAPSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BatteryAnimation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BatteryAnimation
+//
+//  Created by GAPSD on 2018/10/23.
+//  Copyright © 2018年 GAPSD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
